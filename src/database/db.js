5272b16a88d36975f68d52aabee70ac40f57cafa
LarisseLima <larisse.lima2@gmail.com>
2020-08-07 20:32:26 -0300

    Refatora

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,8 @@
 const Database = require('sqlite-async');
 
 function execute(db) {
-    // Criar as tabelas do banco de dados
-    return db.exec(`
+  // Criar as tabelas do banco de dados
+  return db.exec(`
       CREATE TABLE IF NOT EXISTS proffys (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT, 
@@ -10,12 +10,14 @@ function execute(db) {
         whatsapp TEXT,
         bio TEXT
       );
+      
       CREATE TABLE IF NOT EXISTS classes (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         subject INTEGER,
         cost TEXT,
         proffy_id INTEGER
       );
+      
       CREATE TABLE IF NOT EXISTS class_schedule (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         class_id INTEGER,
